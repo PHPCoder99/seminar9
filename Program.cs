22da4ee94ca83d1f845ee29c0ed996ce21b7c85a
Zo PHPCoder99 <gergenkopzoya@gmail.com>
2023-03-11 18:39:17 +0700

    solved problem 3

--- Program.cs
+++ Program.cs
@@ -4,9 +4,19 @@ namespace seminar9
 {
     class Program
     {
+        static int ReadInt(string prompt){
+            Console.WriteLine(prompt);
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        static int SumDigits(int num){
+            if (num == 0) return 0;
+            return (num % 10 + SumDigits(num / 10));
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write(SumDigits(ReadInt("Enter number: ")));
         }
     }
 }
