dd8946035f6921b96dffecc5c6c4e7a1c1cfceed
Zo PHPCoder99 <gergenkopzoya@gmail.com>
2023-03-11 18:41:22 +0700

    solved problem 2

--- Program.cs
+++ Program.cs
@@ -4,9 +4,26 @@ namespace seminar9
 {
     class Program
     {
+        static int ReadInt(string prompt){
+            Console.WriteLine(prompt);
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        static void WriteNumbers(int m, int n){
+            if (n == m-1) return;
+            WriteNumbers(m, n-1);
+            Console.Write(n+" ");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int m = ReadInt("Введите число m: ");
+            int n = ReadInt("Введите число n: ");
+            if (m < n){
+                WriteNumbers(m, n);
+            } else {
+                WriteNumbers(n, m);
+            }
         }
     }
 }
