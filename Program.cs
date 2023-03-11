7ea9d58ae2dae9d205a137d53b7f941f363bdbb3
Zo PHPCoder99 <gergenkopzoya@gmail.com>
2023-03-11 19:02:33 +0700

    solved problem 4

--- Program.cs
+++ Program.cs
@@ -4,9 +4,23 @@ namespace seminar9
 {
     class Program
     {
+        // Задача 69: Напишите программу, которая на вход принимает 
+        // два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+        // A = 3; B = 5 -> 243 (3⁵)
+        // A = 2; B = 3 -> 8
+        static int ReadInt(string prompt){
+            Console.WriteLine(prompt);
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        static int RecursionPower(int a, int b){
+            if (b == 0) return 1;
+            return a * RecursionPower(a, b-1);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(RecursionPower(ReadInt("Введите число а:"), ReadInt("Введите число б:")));
         }
     }
 }
